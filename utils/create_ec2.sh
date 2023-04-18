c10cbb24921946d8a9e2e3f204e1ddfd51fae6ce
maneh.harutyunyan@helpsystems.com <maneh.harutyunyan@helpsystems.com>
2023-04-18 20:40:04 +0400

    added create sg, vpc and ec2 scripts

--- utils/create_ec2.sh
+++ utils/create_ec2.sh
@@ -3,7 +3,28 @@
 # source create_subnet.sh
 # source create_sg.sh
 
-#!/bin/bash
+source create_vpc.sh
+
+# creates an ec2 instance in a vpc
+create_ec2_instance() {
+	local key=$1
+	local ami_id=$2
+	local subnet=$2
+	local ports_string=$2
+
+	# create the vpc
+	create_vpc $ports_string $subnet
+
+	# create ec2 instance in the vpc
+	aws ec2 run-instances \
+		--image-id $ami_id \
+		--count 1 \
+		--instance-type t2.micro \
+		--key-name $key \
+		--subnet-id $subnet_id \
+		--security-group-ids $security_group_ids \
+		&& echo "created an ec2 instance" || exit 1
+}
 
 subnet_id=$1
 sg_id=$2
@@ -12,22 +33,25 @@ if [ -z "$subnet_id" ]; then
     echo "Invalid argument is provided."
     return 1
 else
-    ## create_subnet.sh - ստեղ կանչում ենք սկրիպտը համապատասխան, հետո տակինով վերցնում ու պահում vpc_id-ն subnet_vpc_id-ի մեջ
+    # get vpc_id from subnet
     subnet_vpc_id=$(aws ec2 describe-subnets --subnet-ids "$subnet_id" --query 'Subnets[0].VpcId' --output text 2>/dev/null)
 
+    # cleck for blank case
     if [ -z "$sg_id" ]; then
         echo "Invalid argument is provided"
         return 1
     else
-        ## create_sg.sh - ստեղ կանչում ենք սկրիպտը համապատասխան, հետո տակինով վերցնում ու պահում vpc_id-ն sg_vpc_id-ի մեջ
+        # get vpc_id from sg
         sg_vpc_id=$(aws ec2 describe-security-groups --group-ids "$sg_id" --query 'SecurityGroups[0].VpcId' --output text 2>/dev/null)
         
+        # rice an error if vpc_ids differ
         if [ "$subnet_vpc_id" != "$sg_vpc_id" ]; then
             echo "VPC IDs don't match"
             exit 1
         else
-            matching_vpc_id="$subnet_vpc_id"
-            echo "VPC IDs match: $matching_vpc_id"
+            # store the vpc_id
+            vpc_id="$subnet_vpc_id"
+            echo "VPC IDs match: $vpc_id"
         fi
     fi
 fi
--- utils/create_sg.sh
+++ utils/create_sg.sh
@@ -1,1 +1,55 @@
 #!/bin/bash
+
+# splits string to list by ":"
+function split_string_to_list() {
+  local IFS=':'    
+  read -ra parts <<< "$1"    
+  echo "${parts[@]}"    
+}
+
+# creates a segurity group with a given vpc id and ports
+# ports are splitted by colons (:)
+create_sg_instance() {
+    local vpc_id=$1
+    local port=$2
+
+    # create security group
+    sg_id=$(aws ec2 create-security-group \
+    	--group-name Port22SecurityGroup \
+    	--description "Port 22 security group" \
+    	--vpc-id $vpc_id \
+    	--query 'GroupId' \
+    	--output text) \
+    	&& echo "Port $port Security Group created" || exit 1
+    
+    # open ingress rules with a given port
+    aws ec2 authorize-security-group-ingress \
+    	--group-id $sg_id \
+    	--protocol tcp \
+    	--port $port \
+    	--cidr 0.0.0.0/0 \
+    	&& echo "opened ingress rules for the port $port" || exit 1
+}
+
+# get security groups
+function create_sg() {
+    local vpc_id=$1
+    local ports=$2
+
+	# split the ports
+	local ports_list=$(split_string_to_list "$ports")
+	echo "ports are splited $ports_list"
+
+	# the list of security groups
+	local security_group_ids=()
+
+	# create sg for each port and store it into a list
+	for port in $ports_list
+	do
+		local sg=$(create_sg_instance "$vpc_id" "$port")
+		security_group_ids+=("$sg")
+	done 
+
+	# return sgs list
+	echo "security groups are created ${security_group_ids[@]}"
+}
