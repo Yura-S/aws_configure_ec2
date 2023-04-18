e289fdb7ae880a82b86b03b1421cf6c61df67947
sahak <sahak.azatyan@gmail.com>
2023-04-18 17:42:01 +0400

    Replaced exit with return statement

--- utils/create_ec2.sh
+++ utils/create_ec2.sh
@@ -10,14 +10,14 @@ sg_id=$2
 
 if [ -z "$subnet_id" ]; then
     echo "Invalid argument is provided."
-    exit 1
+    return 1
 else
     ## create_subnet.sh - ստեղ կանչում ենք սկրիպտը համապատասխան, հետո տակինով վերցնում ու պահում vpc_id-ն subnet_vpc_id-ի մեջ
     subnet_vpc_id=$(aws ec2 describe-subnets --subnet-ids "$subnet_id" --query 'Subnets[0].VpcId' --output text 2>/dev/null)
 
     if [ -z "$sg_id" ]; then
         echo "Invalid argument is provided"
-        exit 1
+        return 1
     else
         ## create_sg.sh - ստեղ կանչում ենք սկրիպտը համապատասխան, հետո տակինով վերցնում ու պահում vpc_id-ն sg_vpc_id-ի մեջ
         sg_vpc_id=$(aws ec2 describe-security-groups --group-ids "$sg_id" --query 'SecurityGroups[0].VpcId' --output text 2>/dev/null)
